3ae784c3cdf11a17c925517ac0349891636405d7
bivvo <bivvo.accounts@gmail.com>
2011-10-02 23:57:49 -0700

    Adding a c# string calc. This is a quick and dirty run through. After pairing through it earlier today in the coffee shop.

---
+++ Katas/StringCalculator/c#/10-2-11/StringCalculator/StringCalculator.Test/StringCalcTest.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace StringCalculator.Test
+{
+    [TestFixture]
+    public class StringCalcTest
+    {
+        private Calculator _calculator = new Calculator();
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TestCase]
+        public void Given_an_empty_string_Result_should_be_zero()
+        {
+            _calculator.Add(string.Empty).ShouldBe(0);
+        }
+
+        [TestCase]
+        public void Give_a_single_value_Result_to_equal_given_value()
+        {
+            _calculator.Add("1").ShouldBe(1);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("2,3", 5)]
+        [TestCase("10,10", 20)]
+        [TestCase("100,100,100", 300)]
+        public void Given_delimited_string_of_two_values_Then_return_value_should_be_the_sum(string input, int expected)
+        {         
+            _calculator.Add(input).ShouldBe(expected);
+        }
+
+        [TestCase("1\n2", 3)]
+        [TestCase("1\n2,3", 6)]
+        public void Given_string_delimeted_with_newlines_value_should_equal_the_sum_of_all_integers(string input, int expected)
+        {
+            _calculator.Add(input).ShouldBe(expected);
+        }
+
+        [TestCase("1\n,")]
+        [TestCase("1,3,4,\n5")]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void Given_Double_Delimited_string_Then_calculator_should_return_argumentexception(string input)
+        {
+            _calculator.Add(input);
+        }
+
+        [TestCase("//;1;2;3", 6)]
+        [TestCase("//;1;2;3,6", 12)]
+        [TestCase("//;1;2;3,6\n6", 18)]
+        public void Given_a_change_of_delimiter_calculator_Should_handle_new_delimeter_and_return_sum(string input, int expected)
+        {
+            _calculator.Add(input).ShouldBe(expected);
+        }
+
+        [TestCase]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void Given_negative_values_calculator_should_throw_argument_exception()
+        {
+            _calculator.Add("-1,2,3");
+        }
+
+    }
+}
---
+++ Katas/StringCalculator/c#/10-2-11/StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        List<char> _splitBy = new List<char>() { ',', '\n' };
+        public int Add(string input)
+        {
+            if (input.Equals(string.Empty)) return 0;
+
+            if (input.ToCharArray().Any(x => x == ',' || x == '\n') || isCustomDelimited(input) )
+                return InputToIntArray(input).Sum();
+
+            return int.Parse(input);
+
+        }
+
+        private bool isCustomDelimited(string input)
+        {
+            return input.StartsWith("//");
+        }
+
+        public int[] InputToIntArray(string input)
+        {
+            input = handleCustomDelimited(input);
+            var segments = input.Split(_splitBy.ToArray());
+            if (isInvalidSegment(segments))
+                throw new ArgumentException();
+            var parsedSegments = segments.Select(x => int.Parse(x)).ToArray();
+            if(isNegativeValue(parsedSegments))
+                throw new ArgumentException();
+
+            return parsedSegments;
+
+        }
+
+        private string handleCustomDelimited(string input)
+        {
+            if (isCustomDelimited(input))
+            {
+                _splitBy.Add(input[2]);
+                input = input.Substring(3);
+            }
+            return input;
+        }
+
+        private bool isNegativeValue(int[] parsedSegments)
+        {
+            return parsedSegments.Any(x => x < 0);
+        }
+
+        private bool isInvalidSegment(string[] segments)
+        {
+            return segments.Any(x => x == string.Empty);
+        }
+    }
+}
